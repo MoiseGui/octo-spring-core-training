44825668efbe9a5fcc53fdf6a01e88d118073fb2
Moïse Gui <k.moise.gui@gmail.com>
2022-03-02 19:35:27 +0100

    H2 config done

--- aop/src/main/java/ma/octo/aop/config/AppConfig.java
+++ aop/src/main/java/ma/octo/aop/config/AppConfig.java
@@ -6,8 +6,8 @@ import org.springframework.context.annotation.ComponentScan.Filter;
 @Configuration
 @EnableAspectJAutoProxy
 @ComponentScan(value = "ma.octo",
-    excludeFilters = @Filter(type = FilterType.REGEX, pattern = "ma.octo.aop.entity.*"))
+        excludeFilters = @Filter(type = FilterType.REGEX, pattern = "ma.octo.aop.entity.*"))
 @PropertySource("classpath:application.properties")
-@Import({DevConfigProperties.class, ProdConfigProperties.class})
+@Import({DevConfigProperties.class, ProdConfigProperties.class, H2Config.class})
 public class AppConfig {
 }
---
+++ aop/src/main/resources/sql/create-table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `languages`
+(
+    `id`             varchar(20) NOT NULL,
+    `name`           varchar(20) NOT NULL,
+    `author`         varchar(100) NOT NULL,
+    `file_extension` varchar(20) NOT NULL,
+    PRIMARY KEY (`id`)
+);
